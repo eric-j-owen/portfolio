43deff4b48d62de408384f6e0ef65abc6f59486e
eric-j-owen <145154417+eric-j-owen@users.noreply.github.com>
2024-10-16 16:06:38 -0400

    changes config, adds contact css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://eric-j-owen.github.io",
-  base: "portfolio",
-});
+export default defineConfig({});
