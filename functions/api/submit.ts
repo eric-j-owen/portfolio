036000c666d4a5208ec34fd707dbce23f89a0c18
eric-j-owen <145154417+eric-j-owen@users.noreply.github.com>
2024-10-22 10:07:16 -0400

    adds types to worker

--- functions/api/submit.ts
+++ functions/api/submit.ts
@@ -1,6 +1,8 @@
-export async function onRequestPost(context) {
+import { Request } from "@cloudflare/workers-types";
+
+export async function onRequestPost(context: { request: Request }) {
   try {
-    let input = await context.request.formDate();
+    let input = await context.request.formData();
     let pretty = JSON.stringify([...input], null, 2);
     return new Response(pretty, {
       headers: {
